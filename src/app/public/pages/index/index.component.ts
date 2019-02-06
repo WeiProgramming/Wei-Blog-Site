dd6f8d7754ea6c85158bd4a64e97cf7468a6cf91
Wei Leung <wei.leung1995@gmail.com>
2019-02-06 02:23:13 -0800

    took out card functionality and added delay to navigate

--- src/app/public/pages/index/index.component.ts
+++ src/app/public/pages/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-index',
@@ -8,61 +9,26 @@ import {BehaviorSubject, Observable} from 'rxjs';
 })
 export class IndexComponent implements OnInit {
   // homeSubject = new BehaviorSubject<boolean>(false);
-  // personalSubject = new BehaviorSubject<boolean>(false);
-  // businessSubject = new BehaviorSubject<boolean>(false);
-  // randomSubject = new BehaviorSubject<boolean>(false);
   // homeOpen = this.homeSubject.asObservable();
-  // personalOpen = this.personalSubject.asObservable();
-  // businessOpen = this.businessSubject.asObservable();
-  // randomOpen = this.randomSubject.asObservable();
-  constructor() { }
-
+  constructor(private router: Router) {
+  }
   ngOnInit() {
     const items = document.querySelector('.category-group');
-    // items.addEventListener('click', this.test);
   }
-  // test(event) {
-  //   const name = event.target.getAttribute('data-name');
-  //   console.log(name);
-  //   if (name === 'home') {
-  //     this.homeSubject.next(true);
-  //     this.personalSubject.next(false);
-  //     this.businessSubject.next(false);
-  //     this.randomSubject.next(false);
-  //   } else if (name === 'personal') {
-  //     this.homeSubject.next(false);
-  //     this.personalSubject.next(true);
-  //     this.businessSubject.next(false);
-  //     this.randomSubject.next(false);
-  //   } else if (name === 'business') {
-  //     this.homeSubject.next(false);
-  //     this.personalSubject.next(false);
-  //     this.businessSubject.next(true);
-  //     this.randomSubject.next(false);
-  //   } else if (name === 'random') {
-  //     this.homeSubject.next(false);
-  //     this.personalSubject.next(false);
-  //     this.businessSubject.next(false);
-  //     this.randomSubject.next(true);
-  //   }
-  //   // console.log(`personal ${this.personalOpen}, home ${this.homeOpen}, business ${this.businessOpen}, random ${this.randomOpen}`);
-  //   console.log(this.homeOpen.subscribe(res => console.log(res + 'hey there')));
-  // }
+
   expand(event) {
     const target = event.target || event.srcElement || event.currentTarget;
     const items = document.querySelectorAll('.category-item');
     // @ts-ignore
     items.forEach( item => {
-      console.log(item.childNodes[0]);
       item.style.flex = 1;
       item.classList.remove('active');
-      item.childNodes[2].classList.add('hidden');
-      target.childNodes[2].classList.remove('card-container');
     });
     target.style.flex = 8;
     target.classList.add('active');
-    target.childNodes[2].classList.remove('hidden');
-    target.childNodes[2].classList.add('card-container');
+    setTimeout(() => {
+      this.router.navigate([`/${event.target.childNodes[0].innerText.toLowerCase()}`]);
+    }, 600);
   }
 
 }
